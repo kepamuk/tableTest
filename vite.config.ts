79b86b7ca624a5cae427a472ed74bea4e3469b5f
kepamuk <kepamuk@yandex.ru>
2025-03-13 17:22:03 +0300

    123

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 
 export default defineConfig({
-  base: '/tableForData',
+  base: '/tableTest',
   plugins: [react()],
   resolve: {
     alias: {
